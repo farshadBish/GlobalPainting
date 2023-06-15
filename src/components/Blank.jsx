13dfd8c5e4aa222638c649ab7f332c91e1086c39
farshadBish <farshadota@gmail.com>
2023-06-15 18:22:13 +0200

    for phone

--- src/components/Blank.jsx
+++ src/components/Blank.jsx
@@ -5,9 +5,26 @@ const Blank = () => {
 
   const canvasRef = useRef(null)
   const contextRef = useRef(null)
-  const [isDrawing,setIsDrawing] = useState(false)
+  const [isDrawing,setIsDrawing] = useState(false);
+  const [history,setHistory] = useState([])
 
-  useEffect(() => {
+  const fetchHistory = async () => {
+    try {
+      let response = await fetch(`http://localhost:3000`, {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      let data = await response.json();
+      setHistory(data)
+      console.log(history,"THIS IS HISTOR");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect( () => {
     const canvasZone = document.getElementById("paintingZone")
     console.log(canvasZone);
     const canvas = canvasRef.current;
@@ -22,7 +39,16 @@ const Blank = () => {
     context.strokeStyle = "black";
     context.lineWidth = 5;
     contextRef.current = context;
+    fetchHistory();
+
   }, [])
+
+  useEffect(()=>{
+    history.forEach(element => {
+      contextRef.current.lineTo(...element, );
+      contextRef.current.stroke();
+    });
+  },[history])
   
 
 
@@ -44,18 +70,22 @@ const Blank = () => {
     const {offsetX , offsetY} = nativeEvent;
     contextRef.current.lineTo(offsetX,offsetY);
     contextRef.current.stroke();
+    console.log(offsetX,offsetY);
   }
 
   return (
     <div className="">
     <canvas 
-    className="bg-white rounded-sm shadow-2xl"
+    className="bg-white  rounded-sm shadow-2xl"
     id="paintingZone"
     width={1050}
     height={500}
     onMouseDown={startDrawing}
+    onTouchStart={startDrawing}
     onMouseUp={endDrawing}
+    onTouchEnd={endDrawing}
     onMouseMove={draw}
+    onTouchMove={draw}
     ref={canvasRef}
     ></canvas>
         </div>

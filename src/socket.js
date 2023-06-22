27e056811d7eb90e99ce407bbacfaa314ab3fe26
ruzi <rozbeh24@outlook.com>
2023-06-22 03:38:38 +0330

    socket url

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import config from './config.json'
 import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
-const {BASE_URL,SOCKET_PATH}=config
-const URL = BASE_URL
+const {SOCKET_URL,SOCKET_PATH}=config
+const URL = SOCKET_URL
 
 export const socket = io(URL,{path:SOCKET_PATH});
